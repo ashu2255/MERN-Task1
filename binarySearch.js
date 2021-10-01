6d009e5b362a4e9838051332cc664e13bec9c990
Ayesha Harun Sayyad <ayeshasayyad2255@gmail.com>
2021-10-01 16:26:33 +0530

    Binary search program is created

---
+++ binarySearch.js
@@ -0,0 +1,28 @@
+
+    const binary = (val , arr) =>{
+
+        let lower = 0;
+        let upper = arr.length - 1;
+
+        while(lower <= upper){
+            const middle = lower + Math.floor((upper-lower)/2);
+            if(val === arr[middle]){
+                console.log('value present at postion:'+arr[middle])
+                return middle;
+                
+            }
+
+            if(val < arr[middle]){
+             upper = middle -1;
+            }
+            else{
+                lower = middle+1;
+            }
+        }
+        return -1;
+    }
+
+const values = [0,1,2,3,4,5,6,7,8,9,10];
+console.log (binary(5, values));
+console.log (binary(1, values));
+console.log (binary(10, values));
