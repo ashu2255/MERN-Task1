58beb27e804a0944c94d815f34b88bbd4d3880ac
Ayesha Harun Sayyad <ayeshasayyad2255@gmail.com>
2021-10-04 10:01:54 +0530

    fizzbuzz with test case

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,13 +1,16 @@
-for (let i=1; i<=30 ; i++){
-    if(i % 5 === 0 && i % 3 === 0){
-        console.log("FizzBuzz");
+function fizzbuzz(number) {
+    //for (let i=1; i<=30 ; i++){
+    if (number % 3 == 0 && number % 5 == 0) {
+      return 'FizzBuzz';
+    } else if (number % 3 == 0) {
+      return 'Fizz';
+    } else if (number % 5 == 0) {
+      return 'Buzz';
+    } else {
+      return number;
     }
-    else if(i % 3 ===0 ){
-        console.log("Fizz");
-    }else if(i % 5 ===0){
-        console.log("Buzz");
-    }
-    else{
-    console.log(i)
-}
-}
+    //}
+  };
+  
+  module.exports = fizzbuzz;
+
